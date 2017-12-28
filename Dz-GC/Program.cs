819c9de44d97a647284b4b2374c78c59a85c8726
Jeka Lion <djoni-01@outlook.com>
2017-12-28 19:32:53 +0600

    init

---
+++ Dz-GC/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz_GC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<TestClass> test = new List<TestClass>();
+            TestClass classTemp = new TestClass();
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                test.Add(classTemp);
+                GC.Collect();
+            }
+            //Console.WriteLine("Поколение коллекции 'test - '" + GC.GetGeneration(test));
+            //{
+            //    int Temp = 0;
+            //    Console.WriteLine("Поколение переменной 'Temp' - " + GC.GetGeneration(Temp));
+            //}
+
+            //Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
